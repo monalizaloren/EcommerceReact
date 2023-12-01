ead419aaea13ed1879911a41979e0f3bd43b41e1
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-12-01 11:06:06 -0300

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,40 @@
+export default {
+  users: [
+    { username: "regular", accessLevel: 1, password: "password" },
+    { username: "admin", accessLevel: 0, password: "password" }
+  ],
+  initProducts: [
+    {
+      name: "sapatos",
+      stock: 10,
+      price: 399.99,
+      shortDesc: " Nossa coleção de sapatos é projetada para combinar estilo e conforto. Oferecemos uma variedade de estilos, desde sapatos formais e elegantes até tênis casuais e sandálias estilosas.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    },
+    {
+      name: "bolsas",
+      stock: 20,
+      price: 299.99,
+      shortDesc: "Nossas bolsas são o acessório perfeito para complementar qualquer look. Temos uma seleção abrangente de estilos, desde bolsas de ombro espaçosas até bolsas transversais práticas e clutches sofisticadas.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    },
+    {
+      name: "blusas",
+      stock: 15,
+      price: 149.99,
+      shortDesc: "Nossas roupas são projetadas para atender às necessidades de homens e mulheres modernos, que buscam um visual elegante e atualizado.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    },
+    {
+      name: "shorts",
+      stock: 5,
+      price: 109.99,
+      shortDesc: "Nossos shorts são ideais para os dias quentes de verão ou para atividades esportivas. Oferecemos uma ampla seleção de modelos, desde shorts casuais até shorts de corrida e esportivos.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    }
+  ]
+};
